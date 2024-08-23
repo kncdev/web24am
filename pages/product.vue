3ee7553eeb47b1a2c90ada7a61c881dcd814e951
kncdev <akangnatar@gmail.com>
2024-08-23 08:19:36 +0700

    Update product.vue

--- pages/product.vue
+++ pages/product.vue
@@ -2,6 +2,10 @@
   <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
     <h1 class="text-5xl font-bold mb-8">Our Products</h1>
     <p class="text-lg mb-4">Here you can find information about our products.</p>
-    <img src="/_nuxt/assets/images/product-image.png" alt="Product" class="w-1/2 h-auto rounded shadow-md" />
+    <img :src="product1" alt="Product" class="w-1/2 h-auto rounded shadow-md" />
   </div>
 </template>
+
+<script setup>
+import product1 from '~/assets/images/product-image.png'
+</script>

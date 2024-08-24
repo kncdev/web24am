da566bceac3c5e5b60e511ccfa8c98fe406d7549
kncdev <akangnatar@gmail.com>
2024-08-24 12:05:19 +0700

    Delete pages directory

--- pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-    <h1 class="text-5xl font-bold mb-8">Welcome to Home Page</h1>
-    <p class="text-lg">This is the home page of your Nuxt 3 app.</p>
-  </div>
-</template>
